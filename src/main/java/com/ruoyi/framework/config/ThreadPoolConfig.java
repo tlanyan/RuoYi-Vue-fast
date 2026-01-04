b00874517cec592cf0956cc8320327b37a79aae5
tlanyan <tlanyan@hotmail.com>
2026-01-04 10:21:51 +0800

    feat: use virtual thread

--- src/main/java/com/ruoyi/framework/config/ThreadPoolConfig.java
+++ src/main/java/com/ruoyi/framework/config/ThreadPoolConfig.java
@@ -4,7 +4,11 @@ import com.ruoyi.common.utils.Threads;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -40,6 +44,28 @@ public class ThreadPoolConfig
         // 线程池对拒绝任务(无线程可用)的处理策略
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return executor;
+    }
+
+    /**
+     * 虚拟线程执行器 (Java 21+)
+     * 适用于I/O密集型任务，如HTTP请求、数据库查询、文件操作等
+     * 虚拟线程非常轻量，可以创建数百万个而不会耗尽系统资源
+     */
+    @Bean(name = "virtualThreadExecutor")
+    public ExecutorService virtualThreadExecutor()
+    {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+
+    /**
+     * Spring TaskExecutor适配器，用于@Async注解
+     * 当需要使用虚拟线程执行异步任务时，可以指定此执行器：
+     * @Async("virtualTaskExecutor")
+     */
+    @Bean(name = "virtualTaskExecutor")
+    public TaskExecutor virtualTaskExecutor(ExecutorService virtualThreadExecutor)
+    {
+        return new TaskExecutorAdapter(virtualThreadExecutor);
     }
 
     /**
--- src/main/java/com/ruoyi/framework/manager/AsyncManager.java
+++ src/main/java/com/ruoyi/framework/manager/AsyncManager.java
@@ -1,6 +1,7 @@
 package com.ruoyi.framework.manager;
 
 import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import com.ruoyi.common.utils.Threads;
@@ -8,7 +9,8 @@ import com.ruoyi.common.utils.spring.SpringUtils;
 
 /**
  * 异步任务管理器
- * 
+ * 使用虚拟线程执行异步任务 (Java 21+)
+ *
  * @author ruoyi
  */
 public class AsyncManager
@@ -19,9 +21,14 @@ public class AsyncManager
     private final int OPERATE_DELAY_TIME = 10;
 
     /**
-     * 异步操作任务调度线程池
+     * 异步操作任务调度线程池 (用于定时调度)
      */
-    private ScheduledExecutorService executor = SpringUtils.getBean("scheduledExecutorService");
+    private ScheduledExecutorService scheduler = SpringUtils.getBean("scheduledExecutorService");
+
+    /**
+     * 虚拟线程执行器 (用于实际任务执行, Java 21+)
+     */
+    private ExecutorService virtualExecutor = SpringUtils.getBean("virtualThreadExecutor");
 
     /**
      * 单例模式
@@ -36,13 +43,24 @@ public class AsyncManager
     }
 
     /**
-     * 执行任务
-     * 
+     * 执行任务 (使用虚拟线程)
+     * 调度由传统线程池处理，实际执行由虚拟线程执行器处理
+     *
      * @param task 任务
      */
     public void execute(TimerTask task)
     {
-        executor.schedule(task, OPERATE_DELAY_TIME, TimeUnit.MILLISECONDS);
+        scheduler.schedule(() -> virtualExecutor.execute(task), OPERATE_DELAY_TIME, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 立即执行任务 (使用虚拟线程，无延迟)
+     *
+     * @param task 任务
+     */
+    public void executeNow(Runnable task)
+    {
+        virtualExecutor.execute(task);
     }
 
     /**
@@ -50,6 +68,7 @@ public class AsyncManager
      */
     public void shutdown()
     {
-        Threads.shutdownAndAwaitTermination(executor);
+        Threads.shutdownAndAwaitTermination(scheduler);
+        Threads.shutdownAndAwaitTermination(virtualExecutor);
     }
 }
